52f45c9be045517fe47debfd17189f5ae6ad0d4a
mateus24 <141456405+Mateusfreitas24@users.noreply.github.com>
2023-08-11 17:41:22 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(600, 600);
+  background("rgb(0,0,0)")
+}
+
+function draw() {
+  
+  fill("#3900FF");
+  stroke("WHITE");
+  ;
+  
+  if(mouseIsPressed){rect(mouseX, mouseY, 50, 50)
+    
+  }
+  
+}
